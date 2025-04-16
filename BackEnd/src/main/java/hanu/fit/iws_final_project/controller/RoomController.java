2fa7b06d4f98b5f52a83edd7fabf6c915de307e8
TuyetNguyen15 <Yang2004tuyet@gmail.com>
2025-04-16 12:59:16 +0700

    update test

--- BackEnd/src/main/java/hanu/fit/iws_final_project/controller/RoomController.java
+++ BackEnd/src/main/java/hanu/fit/iws_final_project/controller/RoomController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//hi
 @RestController
 @RequestMapping("/api")
 public class RoomController {
