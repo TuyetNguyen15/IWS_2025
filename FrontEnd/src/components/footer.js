c81f9868e6874f66b2ca684f5f48d5793aacc26a
Lan <146980437+Lan071004@users.noreply.github.com>
2025-04-17 23:54:14 +0700

    Fix footer style

--- FrontEnd/src/components/footer.js
+++ FrontEnd/src/components/footer.js
@@ -1,23 +1,24 @@
 import React from "react";
 import './styles.css';
-const Footer=()=>{
-    return(
-        <div class="container">
-        <div class="element">
-            <i class="fas fa-phone"></i>
-            Hotline: 1800-1234
-        </div>
 
-        <div class="element">
-            © Created by Mercy Company | All rights reserved.
-        </div>
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="element">
+        <i className="fas fa-phone"></i>
+        Hotline: 1800-1234
+      </div>
 
-        <div class="element">
-            <i class="fas fa-map-marker-alt"></i>
-            Số 10, Quận Cầu Giấy, Hà Nội
-        </div>
+      <div className="element">
+        © Created by Mercy Company | All rights reserved.
+      </div>
+
+      <div className="element">
+        <i className="fas fa-map-marker-alt"></i>
+        Số 10, Quận Cầu Giấy, Hà Nội
+      </div>
     </div>
-    )
+  )
 }
-export  default Footer;
 
+export default Footer;
